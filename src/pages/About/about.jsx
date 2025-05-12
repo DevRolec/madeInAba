28b022bc70b2a5a12d111318f777bfda5d146d00
Favour-create <ezeanyagufavour06@gmail.com>
2025-05-12 03:20:45 -0700

    My own version

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -3,7 +3,42 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <h1>About Us</h1>
+      <div>
+       <p>
+          Jenga Tech, a dynamic tech organisation based in Okigwe Road, Aba, Abia State, Nigeria, is making significant strides in the
+          digital lanscape by training young people in technology skills and developing cutting-edge software
+          solutions.
+       </p>
+       <br />
+       <br />
+       <p>
+            Jenga Tech is focused on equipping individuals with skills in full-stack development, digital marketing, 
+            product design, data analysis, data science and cyber security. Jenga tech is rapidly becoming a hub for 
+            innovation and digital empowerment in southeastern Nigeria.
+       </p>
+       <br />
+       <br />
+       <p>
+            At the core of its mission is capacity building- offering intensive, hands-on training programs that bridge
+            the skills gap and open doors to global opportunities. Through workshops, bootcamps and mentorship, the 
+            organisation nurtures a growing community of tech professionals who are contributing to the digital economy
+            both locally and internationally.
+       </p>
+       <br />
+       <br />
+       <p>
+            Through the development of this platform, we aim to transform how businesses in Aba connect with customers online.
+            This platform not only showcases the technical capability of us, the Jenga tech team, but also reinforces 
+            the organisation's commitment to sustainable economic development through technology.
+       </p>
+       <br />
+       <br />
+       <p>
+            By fostering digital inclusion and innovation, Jenga tech is positioning Aba as a rising tech powerhouse 
+            and contributing to the broader goal of digital transformation in Nigeria.
+       </p>
+      </div>
     </div>
   )
 }
