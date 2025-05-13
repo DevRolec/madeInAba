6afd7c6746019bb83f49445c15758843105bdd73
Kalu Onuka Daniel <danielfrosh22@gmail.com>
2025-05-13 11:53:31 +0100

    My Solutions Version

---
+++ src/pages/Solution/Solution.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Solution = () => {
+  return (
+    <div>
+      <div className="Header">
+        <h1>Solutions</h1>
+      </div>
+      <div className="Solutions">
+        <h4>Solutions</h4>
+        <p className="align">Services tailored to your business</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Solution
